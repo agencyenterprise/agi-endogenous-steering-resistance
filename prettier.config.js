8d72da6a28884728f67e269695ef7cb95bdf80f3
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2025-07-02 14:01:43 -0300

    initial configuration and addition of recharts and framer motion libs

---
+++ prettier.config.js
@@ -0,0 +1,26 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  // Plugins
+  plugins: ["prettier-plugin-tailwindcss", "@trivago/prettier-plugin-sort-imports"],
+
+  // Basic formatting (matching EditorConfig)
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: "lf",
+
+  // Common preferences
+  semi: false,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "avoid",
+
+  // Sort imports
+  importOrder: ["<THIRD_PARTY_MODULES>", "^@/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+}
+
+export default config
