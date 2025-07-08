12c4082f6e4f491a0ed6172e9e6fdf7d73c8e89e
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2025-07-08 16:12:22 -0300

    update table of contents max inner width

--- src/components/table-of-contents.tsx
+++ src/components/table-of-contents.tsx
@@ -26,7 +26,7 @@ export function TableOfContents() {
 
   return (
     <div className="flex-1 w-full h-full max-w-2xl lg:absolute lg:top-0 lg:left-0 lg:max-w-[calc((100vw-var(--container-2xl))/2)] xl:max-w-[calc((100vw-var(--container-3xl))/2)]!">
-      <div className="bg-background flex flex-col gap-4 flex-1 border-b lg:border-b-0 lg:border-r px-8 pb-8 lg:sticky lg:top-0">
+      <div className="bg-background flex flex-col gap-4 flex-1 border-b lg:border-b-0 lg:border-r px-8 pb-8 lg:sticky lg:top-0 lg:ml-auto lg:max-w-64">
         <div className="text-lg font-bold">Contents</div>
 
         <ul className="flex flex-col gap-2">
